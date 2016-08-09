f3f67aecae48a1897ff8c4673f4b7990ac7dd153
Jake Ninness <jake.ninness@gmail.com>
2016-08-10 11:21:45 +1200

    hello world. app working

---
+++ src/app/app.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+@Component({
+
+  selector: 'app',
+  template: `
+  <div>
+    <h3>
+      Yo, World!
+    </h3>
+  </div>
+`
+
+})
+
+export class App{}
